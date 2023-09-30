05d4247d6b64d69b77160bb9d53ba89309bd9abe
sxy15 <131671708+sxy15@users.noreply.github.com>
2023-09-30 03:17:02 +0000

    feat: array

--- chapter_array_and_linkedlist/array.ts
+++ chapter_array_and_linkedlist/array.ts
@@ -3,3 +3,12 @@ const arr: number[] = new Array(5).fill(0);
 console.log('数组 arr =', arr);
 let nums: number[] = [1, 3, 2, 5, 4];
 console.log('数组 nums =', nums);
+
+/* 随机访问元素 */
+function randomAccess(nums: number[]): number {
+    // 在区间 [0, nums.length) 中随机抽取一个数字
+    const random_index = Math.floor(Math.random() * nums.length);
+    // 获取并返回随机元素
+    const random_num = nums[random_index];
+    return random_num;
+}
