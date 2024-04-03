ccb9f6ce9a7530d09125f3f31a1c036757765c71
sxy15 <sxy_19921115@163.com>
2024-04-03 14:01:09 +0800

    Time: 61 ms (63.87%), Space: 52.6 MB (38.6%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -0,0 +1,24 @@
+function isValid(s: string): boolean {
+
+
+  const _stack = []
+
+    for(let i = 0; i < s.length; i++) {
+        const item = s[i]
+
+        if(item === '(' || item === '[' || item === '{') {
+            _stack.push(item)
+        } else {
+            const top = _stack.pop()
+
+            if(item === ')' && top !== '(' ||
+            item === ']' && top !== '[' ||
+            item === '}' && top !== '{' 
+            ) {
+                return false
+            }
+        }
+    }
+
+  return _stack.length === 0
+};
