766af9d0a93c26af83aa859a36803c17a24a7dda
sxy15 <sxy_19921115@163.com>
2024-04-03 16:32:13 +0800

    Time: 852 ms (30.61%), Space: 58.1 MB (96.69%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.ts
@@ -0,0 +1,12 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+
+    while(k > 0) {
+        nums.unshift(nums.pop())
+        k--
+    }
+
+   
+};
