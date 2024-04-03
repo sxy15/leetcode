933c08d66a9dc9b18a5973564b4ac251d54bd4d1
sxy15 <sxy_19921115@163.com>
2024-04-03 19:09:03 +0800

    Time: 203 ms (6.39%), Space: 63.1 MB (44.08%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.ts
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.ts
@@ -2,11 +2,10 @@ function findDuplicate(nums: number[]): number {
     nums.sort((a, b) => a - b)
 
     let n = null
-    for(let i = 1; i < nums.length; i++) {
-        if(nums[i] === nums[i - 1]) {
-            n = nums[i]
-        }
+    for(let item of nums) {
+       if(n === item) {
+        return n
+       }
+       n = item
     }
-
-    return n
 };
