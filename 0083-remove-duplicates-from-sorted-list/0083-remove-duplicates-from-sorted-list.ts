b2b5fb30cf3474a70dd5ac9530d08aef2fb0ed46
sxy15 <sxy_19921115@163.com>
2024-04-09 13:25:52 +0800

    Time: 69 ms (64.73%), Space: 52.3 MB (99.58%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let node = head 
+
+    while(node !== null) {
+        if(node.next !== null && node.val === node.next.val) {
+            node.next = node.next.next
+            continue
+        }
+
+        node = node.next
+    }
+
+    return head
+};
