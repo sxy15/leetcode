cf9823dd35d346ef32e9fb5770418f2758c63c9a
sxy15 <sxy_19921115@163.com>
2024-04-10 19:58:57 +0800

    Time: 77 ms (47.37%), Space: 57.1 MB (18.28%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+    if(!head) return null
+
+    if(head.val === val) {
+        return removeElements(head.next, val)
+    }
+
+    head.next = removeElements(head.next, val)
+
+    return head
+};
