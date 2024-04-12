2266ea19cdfccaee4aed21b0693ac94668bbee65
sxy15 <sxy_19921115@163.com>
2024-04-12 13:36:30 +0800

    Time: 68 ms (35.35%), Space: 53.7 MB (69.79%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+    if(root === null) {
+        return 0
+    }
+
+    let deepLeft = maxDepth(root.left) + 1
+    let deepRight = maxDepth(root.right) + 1
+
+    return Math.max(deepLeft, deepRight)
+};
