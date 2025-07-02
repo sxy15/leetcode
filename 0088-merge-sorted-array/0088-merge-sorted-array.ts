f10519d1d3fb32e3fcbb48a6a9be2611a11a69e2
anfu <sxy_19921115@163.com>
2025-07-02 23:28:33 +0800

    Time: 0 ms (100%), Space: 56.2 MB (50.93%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -1,44 +1,24 @@
-/**
- Do not return anything, modify nums1 in-place instead.
- */
- /**
-    双指针
-    对比 尾部的值
-  */
-/**
- * @param {number[]} nums1
- * @param {number} m
- * @param {number[]} nums2
- * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
- */
-var merge = function(nums1, m, nums2, n) {
-    let i = m - 1;
-    let j = n - 1;
-    let k = m + n - 1;
-    
+function merge(nums1, m, nums2, n) {
+    let i = m - 1; // nums1 有效元素末尾指针
+    let j = n - 1; // nums2 末尾指针
+    let k = m + n - 1; // 合并后数组末尾指针
+
+    // 从后向前合并，优先处理较大元素
     while (i >= 0 && j >= 0) {
-        if (nums1[i] < nums2[j]) {
-            nums1[k] = nums2[j];
-            k--;
-            j--;
-        } else {
+        if (nums1[i] > nums2[j]) {
             nums1[k] = nums1[i];
-            k--;
             i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
         }
+        k--;
     }
-    
+
+    // 若 nums2 还有剩余元素，直接复制到 nums1 前面
     while (j >= 0) {
         nums1[k] = nums2[j];
-        k--;
         j--;
+        k--;
     }
-    
-    // while (i >= 0) {
-    //     nums1[k] = nums1[i];
-    //     k--;
-    //     i--;
-    // }
-};
-
+}
