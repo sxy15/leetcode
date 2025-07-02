faa29dae88510da7e72c4561b454339feb4a86ad
anfu <sxy_19921115@163.com>
2025-07-02 23:55:36 +0800

    Time: 0 ms (100%), Space: 55.9 MB (13.27%) - LeetHub

--- 0027-remove-element/0027-remove-element.ts
+++ 0027-remove-element/0027-remove-element.ts
@@ -1,14 +1,30 @@
-// nums = [3,2,2,3], val = 3
+// 1. 快慢指针
+// function removeElement(nums, val) {
+//     let slow = 0
+//     for(let fast = 0; fast < nums.length; fast++) {
+//         if(nums[fast] !== val) {
+//             nums[slow] = nums[fast]
+//             slow++
+//         }
+//     }
 
-function removeElement(nums: number[], val: number): number {
-    let l = 0
+//     return slow
 
-    for(let i = 0; i < nums.length; i++) {
-        if(nums[i] !== val) {
-            nums[l] = nums[i]
-            l++
+// }
+
+// 2. 双指针（交换）
+function removeElement(nums, val) {
+    let left = 0; 
+    let right = nums.length -1;
+
+    while(left <= right) {
+        if(nums[left] === val) {
+            [nums[left], nums[right]] = [nums[right], nums[left]];
+            right--;
+        } else {
+            left++;
         }
     }
 
-    return l
-};
+    return left;
+}
