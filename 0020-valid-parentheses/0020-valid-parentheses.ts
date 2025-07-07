316977355c002b3bcbd258312242b99453294d4a
anfu <sxy_19921115@163.com>
2025-07-07 15:32:15 +0800

    Time: 6 ms (19.35%), Space: 56.9 MB (67.7%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.ts
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -1,24 +1,46 @@
+// function isValid(s: string): boolean {
+
+
+//   const _stack = []
+
+//     for(let i = 0; i < s.length; i++) {
+//         const item = s[i]
+
+//         if(item === '(' || item === '[' || item === '{') {
+//             _stack.push(item)
+//         } else {
+//             const top = _stack.pop()
+
+//             if(item === ')' && top !== '(' ||
+//             item === ']' && top !== '[' ||
+//             item === '}' && top !== '{' 
+//             ) {
+//                 return false
+//             }
+//         }
+//     }
+
+//   return _stack.length === 0
+// };
+
 function isValid(s: string): boolean {
-
-
-  const _stack = []
+    const _stack = []
 
     for(let i = 0; i < s.length; i++) {
         const item = s[i]
-
-        if(item === '(' || item === '[' || item === '{') {
+        if(item === '{' || item === '[' || item === '(') {
             _stack.push(item)
         } else {
             const top = _stack.pop()
 
             if(item === ')' && top !== '(' ||
-            item === ']' && top !== '[' ||
-            item === '}' && top !== '{' 
-            ) {
-                return false
-            }
+                item === ']' && top !== '[' ||
+                item === '}' && top !== '{' 
+                ) {
+                    return false
+                }
         }
     }
 
-  return _stack.length === 0
-};
+    return _stack.length === 0
+}
